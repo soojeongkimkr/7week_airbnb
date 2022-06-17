c008c1187b02ca8981fef109541cb37e30c8382e
puppyGugu <65080345+puppyGugu@users.noreply.github.com>
2022-06-18 00:05:35 +0900

    로그인, 회원가입 모달레이어 팝업 페이지 생성

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,25 @@
 import React from "react";
+import Signup from "./Signup";
+
+import { useState } from 'react';
 
 const Main = () => {
+  // useState를 사용하여 open상태를 변경한다. (open일때 true로 만들어 열리는 방식)
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
   return (
-    <div>
-      <p>메인</p>
-    </div>
+    <React.Fragment>
+      <button onClick={openModal}>로그인</button>
+      <Signup open={modalOpen} close={closeModal} header="로그인 또는 회원 가입">
+      </Signup>
+    </React.Fragment>
   );
 };
 
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,11 +1,50 @@
 import React from "react";
+import styled, { css } from 'styled-components';
 
-const Signup = () => {
+import '../modal.css';
+
+const Signup = (props) => {
+  // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
+  const { open, close, header } = props;
+  
   return (
-    <div>
-      <p>회원가입</p>
+    // 모달이 열릴때 openModal 클래스가 생성된다.
+    <div className={open ? 'openModal modal' : 'modal'}>
+      {open ? (
+        <section>
+          <header>
+            <button className="close" onClick={close}>
+              &times;
+            </button>
+            {header}
+          </header>
+          <main>
+            {props.children}
+            <p>에어비앤비에 오신 것을 환영합니다.</p>
+            <Form>
+              <Input type="text" required placeholder="ID" />
+              <Input type="password" required placeholder="비밀번호" />
+              <Input type="submit" value="계속" />
+            </Form>
+          </main>
+          {/* <footer>
+            <button className="close" onClick={close}>
+              close
+            </button>
+          </footer> */}
+        </section>
+      ) : null}
     </div>
   );
 };
+
+const Form = styled.form`
+
+`
+
+const Input = styled.input`
+display:block;
+margin:0 auto;
+`
 
 export default Signup;
